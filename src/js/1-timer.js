71f2608bd90eb303b6aff4d96a99e4badfe4fb17
Ira <dudarik86@gmail.com>
2024-09-10 22:09:02 -0600

    add index file

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,15 +1,69 @@
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
 
-import flatpickr from "flatpickr";
-import "flatpickr/dist/flatpickr.min.css";
-const options = {
+
+let chosenDate = null;
+let intervalId = null;
+
+
+const picker = document.querySelector('#datatime-piker');
+const startButton = document.querySelector('[data-start]');
+const daysElement = document.querySelector('[data-days]');
+const hoursElement = document.querySelector('[data-hours]');
+const minutesElement = document.querySelector('[data-manutes]');
+const secondsElement = document.querySelector('[data-seconds]');
+
+startButton.disabled = true;
+
+flatpickr(picker, {
   enableTime: true,
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    console.log(selectedDates[0]);
+    const selectedDate = selectedDates[0];
+    if (selectedDate <= new Date()) {
+      iziToast.error({
+        title: 'Error',
+        message: 'Please choose a date in the future',
+      });
+      startButton.disabled = true;
+    } else {
+     chosenDate = selectedDate;
+      startButton.disabled = false;
+    }
   },
-};
+});
+startButton.addEventListener('click', () => {
+  if (chosenDate) {
+    startButton.disabled = true;
+    picker.disabled = true;
+    startCountdown();
+  }
+});
+function startCountdown() {
+  intervalId = setInterval(() => {
+    const currentTime = new Date();
+    const timeLeft = chosenDate - currentTime;
+    if (timeLeft <= 0) {
+      clearInterval(intervalId);
+      updateTimerDisplay(0, 0, 0, 0);
+      iziToast.success({
+        title: 'Success',
+        message: 'Countdown finished!',
+      });
+      picker.disabled = false;
+    } else {
+      const { days, hours, minutes, seconds } = convertMs(timeLeft);
+      updateTimerDisplay(days, hours, minutes, seconds);
+    }
+  }, 1000);
+}
+
+
+
 function convertMs(ms) {
   // Number of milliseconds per unit of time
   const second = 1000;
@@ -27,6 +81,17 @@ function convertMs(ms) {
   const seconds = Math.floor((((ms % day) % hour) % minute) / second);
 
   return { days, hours, minutes, seconds };
+  
+}
+function updateTimerDisplay(days, hours, minutes, seconds) {
+  daysElement.textContent = addLeadingZero(days);
+  hoursElement.textContent = addLeadingZero(hours);
+  minutesElement.textContent = addLeadingZero(minutes);
+  secondsElement.textContent = addLeadingZero(seconds);
+}
+
+function addLeadingZero(value) {
+  return String(value).padStart(2, '0');
 }
 
 console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
